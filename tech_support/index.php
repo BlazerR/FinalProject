a208187be86ca687029673e2941bb9eb6478207b
BlazerR <br229@njit.edu>
2017-03-17 20:12:24 -0400

    2

--- tech_support/index.php
+++ tech_support/index.php
@@ -14,12 +14,12 @@
 
     <h2>Technicians</h2>    
     <ul>
-        <li><a href="under_construction.php">Update Incident</a></li>
+        <li><a href="Under_Construction.php">Update Incident</a></li>
     </ul>
 
     <h2>Customers</h2>
     <ul>
-        <li><a href="under_construction.php">Register Product</a></li>
+        <li><a href="Under_Construction.php">Register Product</a></li>
     </ul>
     
     </nav>
